b7c4f482faac3c50d1e60a8db004ec0d4ac6ba63
Beat Küng <beat-kueng@gmx.net>
2019-03-10 10:55:20 +0100

    initializeFilter: do not wait for baro or mag to initialize
    attitude estimation works w/o baro or mag (set EKF2_MAG_TYPE to None/MC custom)

--- EKF/ekf.cpp
+++ EKF/ekf.cpp
@@ -213,8 +213,8 @@ bool Ekf::initialiseFilter()
 	}
 
 	// check to see if we have enough measurements and return false if not
-	bool hgt_count_fail = _hgt_counter <= 2u * _obs_buffer_length;
-	bool mag_count_fail = _mag_counter <= 2u * _obs_buffer_length;
+	bool hgt_count_fail = _hgt_counter <= 2u * _obs_buffer_length&&false;
+	bool mag_count_fail = _mag_counter <= 2u * _obs_buffer_length&&false;
 
 	if (hgt_count_fail || mag_count_fail) {
 		return false;
